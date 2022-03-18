2cf7d4179c96285abbc05de3ee549e737d08c1c9
Artur Greziak <alex444@wp.pl>
2022-03-18 23:46:27 +0100

    template literal

---
+++ 1.Chapter/3.Task/script.js
@@ -0,0 +1,5 @@
+// Temat ćwiczenia: “Wypisywanie zmiennych przy pomocy literału szablonu”.Napisz skrypt, w którym należy zadeklarować zmienną consto nazwie liczba przechowującą wartość 16. Przy pomocy tej zmiennej oraz literału szablonu wypisz do konsoli ‘Zadeklarowana liczba: 16’ tak jak pokazano w oczekiwanym wyniku.Oczekiwany wynik:Zadeklarowana liczba: 16
+
+const liczba = 16;
+
+console.log(`Zadeklarowana liczba: ${liczba}`)
