ca7fc3ceb0d034ebdcc7055a4620de82d9ab5d92
Artur Greziak <alex444@wp.pl>
2022-03-19 00:49:29 +0100

    Operator ‘+’ jako operator konkatenacji oraz operator dodawania

---
+++ 1.Chapter/7.Task/script.js
@@ -0,0 +1,6 @@
+// Temat ćwiczenia: “Operator ‘+’ jako operator konkatenacji oraz operator dodawania”.Napisz program, w którym należy zadeklarować dwie zmienne const o nazwach a oraz b:●pierwsza zmienna ma przechowywać ciąg znaków ‘4’,●druga zmienna ma przechowywać wartość 2.Wypisz operację konkatenacji dla tych dwóch zmiennych, następnie poniżej wypisz operację dodawania dla tych dwóch zmiennych, ale w drugim przypadku zastosuj obiekt Number() w celu konwersji ciągu znaków na liczbę.Oczekiwany wynik:426
+
+const a = '4';
+const b = 2;
+console.log(a + b);
+console.log(Number(a) + b);
