c3a59e20620c5d2514643f5e798e56fdc3428330
Artur Greziak <alex444@wp.pl>
2022-03-18 23:35:26 +0100

    Concatination

---
+++ 1.Chapter/2.Task/script.js
@@ -0,0 +1,6 @@
+// Temat ćwiczenia: “Wykorzystanie operatora ‘+’ jako operatora konkatenacji przy wypisywaniu ciągu znaków”.Napisz skrypt, w którym należy zadeklarować zmienną consto nazwie ciagprzechowującą ciąg znaków ‘JavaScript’, następnie wypisz do konsoli tekst JavaScript to skryptowy język programowania przy pomocy tej zmiennej oraz dołączonego tekstu tak jak pokazano w oczekiwanym wyniku.Oczekiwany wynik:
+
+// Temat ćwiczenia: “Wykorzystanie operatora ‘+’ jako operatora konkatenacji przy wypisywaniu ciągu znaków”.
+
+const ciag = 'JavaScript';
+console.log(ciag + ' to skryptowy język programowania');
