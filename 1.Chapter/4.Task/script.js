f114380f473f2746939cb5dce69ec6e68ca5c304
Artur Greziak <alex444@wp.pl>
2022-03-18 23:56:39 +0100

    concatenating a template literal

---
+++ 1.Chapter/4.Task/script.js
@@ -0,0 +1,6 @@
+// Temat ćwiczenia: “Połączenie dwóch ciągów znaków”.Napisz skrypt, w którym należy zadeklarować dwie zmienne consto nazwach ciag_1 oraz ciag_2, pierwszy ma przechowywać ciąg znaków ‘Java’, a drugi ‘Script’. Przy pomocy tych dwóch zmiennych, literału szablony oraz operacji konkatenacji wypisz tekst Połączony ciąg znaków: JavaScript tak jak pokazano w oczekiwanym wyniku.Oczekiwany wynik:Połączony ciąg znaków: JavaScript
+
+const ciag_1 = 'Java';
+const ciag_2 = 'Script';
+
+console.log(`${ciag_1}${ciag_2}`);
