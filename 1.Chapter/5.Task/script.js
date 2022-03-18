1fa10eca5d67d68623b48066278620a0ac6bb427
Artur Greziak <alex444@wp.pl>
2022-03-19 00:18:03 +0100

    Listing variables of different data types using one template literal

---
+++ 1.Chapter/5.Task/script.js
@@ -0,0 +1,6 @@
+// Temat ćwiczenia: “Wypisywanie zmiennych o różnych typach danych przy pomocy jednego literału szablonu”.Napisz skrypt, w którym należy zadeklarować dwie zmienne consto nazwach imie oraz wiek, pierwsza ma przechowywać ciąg znaków ‘Ola’, a druga wartość 22, następnie przy pomocy literału szablonu wypisz na ekran tekst tak jak pokazano w oczekiwanym wyniku.Oczekiwany wynik:Imie: OlaWiek: 22
+
+const imie = 'Ola';
+const wiek = 22;
+
+console.log(`Imię: ${imie}\nWiek: ${wiek}`)
