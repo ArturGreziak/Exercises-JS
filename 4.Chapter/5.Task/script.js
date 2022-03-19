9208b8a03521369f67737a170dd2450e92bdcace
Artur Gręziak <alex4448@wp.pl>
2022-03-19 19:47:08 +0100

    Replacing the values of two variables with the assignment operator '='

--- 4.Chapter/5.Task/script.js
+++ 4.Chapter/5.Task/script.js
@@ -1,8 +1,8 @@
 // Temat ćwiczenia: “Zamiana wartości dwóch zmiennych przy pomocy operatora przypisania ‘=’ “.Napisz skrypt, który zamieni wartości dwóch zmiennych.Pierwsza zmienna przyjmuje wartość 15, druga 10. Nazwy zmiennych możesz dobrać dowolnie.Wypisz wartości zmiennych przed jak i po zamianie na ekran tak jak pokazano w oczekiwanym wyniku.Oczekiwany wynik:Pierwsza liczba przed zamiana: 15Druga liczba przed zamiana: 10Pierwsza liczba po zamiana: 10Druga liczba po zamiana: 15
 
-let liczba_2 = 10;
-
 let liczba_1 = 15;
+
+let liczba_2 = 10;
 
 console.log(`Pierwsza liczba przed zamiana: ${liczba_1}`);
 
