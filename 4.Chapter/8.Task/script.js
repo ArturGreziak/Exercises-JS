dd03dbbf1b239c43b6ec84aa42cb7687a6f187b5
Artur Gręziak <alex4448@wp.pl>
2022-03-19 21:07:31 +0100

    The power operator of '**'

---
+++ 4.Chapter/8.Task/script.js
@@ -0,0 +1,5 @@
+// Temat ćwiczenia: “Operator potęgowania ‘**’”.Napisz skrypt, w którym należy zadeklarować:●zmienną const o nazwie liczba przechowującą wartość 5,●zmienną consto nazwie wykładnik przechowującą wartość 3.Wypisz operację potęgowania tych zmiennych według ich nazewnictwa tak jak pokazano w oczekiwanym wyniku.Wykorzystaj do tego operator potęgowania(**) dostępny w JS.Oczekiwany wynik: 5 ^ 3 = 125
+
+const liczba = 5, wykladnik = 3; 
+
+console.log(liczba + ' ^ ' + wykladnik + ' = ' + (liczba * liczba * liczba));
