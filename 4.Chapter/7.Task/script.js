f4023e4b0686c8451965a7d1ac97f49db2f959fd
Artur Gręziak <alex4448@wp.pl>
2022-03-19 20:56:48 +0100

    Operator of increment '++' and decrement '-'

---
+++ 4.Chapter/7.Task/script.js
@@ -0,0 +1,9 @@
+// Temat ćwiczenia: “Operator inkrementacji ‘++’ oraz dekrementacji ‘--’”.Napisz skrypt, w którym należy zadeklarować zmienną let o nazwie liczba przechowującą wartość 5 oraz należy wykonać następujące operacje:●wypisz wartość tej zmiennej do konsoli,●użyj operacji inkrementacji(++) na tej zmiennej oraz ją wypisz do konsoli,●użyj operacji dekrementacji(--) na tej zmiennej oraz ją wypisz do konsoli.Wszystkie operacje mają zostać wypisane tak jak w oczekiwanym wyniku.Oczekiwany wynik: Liczba: 5Liczba po inkrementacji: 6Liczba po dekrementacji: 5
+
+let liczba = 5; 
+
+console.log(`Liczba: ${liczba}`); 
+liczba++; 
+console.log(`Liczba po inkrementacji: ${liczba}`); 
+liczba--; 
+console.log(`Liczba po dekrementacji: ${liczba}`);
