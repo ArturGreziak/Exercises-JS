1d7c9c2fcf527d34797e0356ffe145acad2a059a
Artur Gręziak <alex4448@wp.pl>
2022-03-19 16:18:25 +0100

    Using the cbrt () method to find the cubic root

---
+++ 3.Chapter/6.Task/script.js
@@ -0,0 +1,5 @@
+// Temat ćwiczenia: “Wykorzystanie metody cbrt() do wyznaczenia pierwiastka sześciennego”.Napisz program, w którym należy zadeklarować zmienną const o nazwie liczbaprzechowującą wartość 27, następnie przy pomocy literałów szablonów oraz metody Math.cbrt() wypisz pierwiastek sześcienny z zadeklarowanej zmiennej tak jak pokazano w oczekiwanym wyniku.Oczekiwany wynik:Pierwiastek szescienny z liczby 27 wynosi: 3
+
+const liczba = 27;
+
+console.log(`Pierwiastek szescienny z liczby ${liczba} wynosi: ${Math.cbrt(liczba)}`)
