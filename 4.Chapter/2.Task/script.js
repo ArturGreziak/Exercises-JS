d878f623569dd8862a8dd1c40bcdcb31f29ca0f0
Artur Gręziak <alex4448@wp.pl>
2022-03-19 18:35:50 +0100

    Relational operator '! =='

---
+++ 4.Chapter/2.Task/script.js
@@ -0,0 +1,7 @@
+// Temat ćwiczenia: “Operator relacyjny ‘!==’”.Napisz skrypt, w którym należy zadeklarować:●zmienną constprzechowującą wynik w postaci true lub false wyrażenia 5 !== 0,●zmienną const przechowującą wynik w postaci true lub false wyrażenia 4 !== 4.Wypisz do konsoli wyniki tych operacji logicznych tak jak pokazano w oczekiwanym wyniku.Oczekiwany wynik:Wartosc wyrazenia logicznego 5 !== 0 : trueWartosc wyrazenia logicznego 4 !== 0 : false
+
+const warunek_1 = 5 !== 0; 
+const warunek_2 = 4 !== 4; 
+
+console.log(`Wartosc wyrazenia logicznego 5 != 0 : ${warunek_1}`); 
+console.log(`Wartosc wyrazenia logicznego 4 != 0 : ${warunek_2}`);
