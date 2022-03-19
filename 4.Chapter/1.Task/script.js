9c5e1b524c97f38a79c42840e88581e78209d958
Artur Gręziak <alex4448@wp.pl>
2022-03-19 18:30:19 +0100

    Relational operator

---
+++ 4.Chapter/1.Task/script.js
@@ -0,0 +1,7 @@
+// Temat ćwiczenia: “Operator relacyjny ‘>’ oraz ‘<’”.Napisz skrypt, w którym należy zadeklarować:●zmienną const przechowującą wynik w postaci true lub false wyrażenia 9 > 2,●zmienną constprzechowującą wynik w postaci true lub false wyrażenia 5 > 7.Wypisz do konsoli wyniki tych operacji logicznych tak jak pokazano w oczekiwanym wyniku.Oczekiwany wynik:Wartosc wyrazenia logicznego 9 > 2 : trueWartosc wyrazenia logicznego 5 > 7 : false
+
+const warunek_1 = 9 > 2; 
+const warunek_2 = 5 > 7; 
+
+console.log(`Wartosc wyrazenia logicznego 9 > 2 : ${warunek_1}`); 
+console.log(`Wartosc wyrazenia logicznego 5 > 7 : ${warunek_2}`);
