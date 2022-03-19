3037f1df619a365f471a1fc70bcdec53882af1ee
Artur Gręziak <alex4448@wp.pl>
2022-03-19 16:15:12 +0100

    Using the sqrt () method to find the square root

---
+++ 3.Chapter/5.Task/script.js
@@ -0,0 +1,5 @@
+// Temat ćwiczenia: “Wykorzystanie metody sqrt() do wyznaczenia pierwiastka kwadratowego”.Napisz program, w którym należy zadeklarować zmienną const o nazwie liczba przechowującą wartość 16, następnie przy pomocy literałów szablonów oraz metody Math.sqrt() wypisz pierwiastek kwadratowy z zadeklarowanej zmiennej tak jak pokazano w oczekiwanym wyniku.Oczekiwany wynik:Pierwiastek kwadratowy z liczby 16 wynosi: 4
+
+const liczba = 16;
+
+console.log(`Pierwiastek kwadratowy z liczby ${liczba} wynosi: ${Math.sqrt(liczba)}`);
