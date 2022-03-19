640b4ee6a427adbd7f434661967d356ba1b4c28a
Artur Greziak <alex444@wp.pl>
2022-03-19 01:31:05 +0100

    Prompt () method - getting a number from the user

---
+++ 2.Chapter/1.Task/script.js
@@ -0,0 +1,5 @@
+// Temat ćwiczenia: “Metoda prompt() - pobieranie liczby od użytkownika”.Napisz skrypt, który będzie pobierał od użytkownika jedną liczbę przy pomocy metody prompt() oraz wartość ta zostanie przypisana do zmiennej typu consto nazwie liczba. Wypisz tą liczbę do konsoli tak jak pokazano w oczekiwanym wyniku.Oczekiwany wynik:Okno wywołane metodą prompt():Konsola:
+
+const liczba = parseInt(prompt());
+
+console.log(liczba);
