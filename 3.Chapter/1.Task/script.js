a25990efa0cf2e4f478701b04a46d3d3bc9142db
Artur Gręziak <alex4448@wp.pl>
2022-03-19 14:05:00 +0100

    Adding two variables of type number

---
+++ 3.Chapter/1.Task/script.js
@@ -0,0 +1,8 @@
+// Temat ćwiczenia: “Dodawania dwóch zmiennych typu number”.Napisz skrypt, w którym należy zadeklarować:●zmienną const o nazwie a przechowującą wartość 15,●zmienną const o nazwie b przechowującą wartość 13,●zmienną const o nazwie wynik, której należy przypisać operację dodawania zmiennej a oraz b.Wypisz do konsoli wartość zmiennej o nazwie wynik tak jak pokazano w oczekiwanym wyniku.Oczekiwany wynik: 28
+
+const a = 15;
+const b = 13;
+
+const wynik = a + b;
+
+console.log(wynik)
