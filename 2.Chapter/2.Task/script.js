37b608e8147eb74a83723c2e54a60c39eda0fbb8
Artur Greziak <alex444@wp.pl>
2022-03-19 01:52:15 +0100

    Prompt () method - retrieving a string from the user

---
+++ 2.Chapter/2.Task/script.js
@@ -0,0 +1,7 @@
+// Temat ćwiczenia: “Metoda prompt() - pobieranie łańcucha znaków od użytkownika”.Napisz skrypt, który będzie pobierał od użytkownika imię przy pomocy metody prompt() oraz łańcuch ten zostanie przypisany do zmiennej typu consto nazwie imie. Wypisz to imię do konsoli tak jak pokazano w oczekiwanym wyniku.Oczekiwany wynik:Okno wywołane metodą prompt():Konsola:
+
+const imie = prompt(`Podaj imię: `);
+
+// console.log(`Podane imie: ${imie}`);
+
+alert(`Podane imie: ${imie}`)
