bee1cc8ba98f2cc43f1b3c9259e6d42188eed61f
Artur Gręziak <alex4448@wp.pl>
2022-03-19 16:23:00 +0100

    Using the abs () method to determine the absolute value

--- 3.Chapter/7.Task/script.js
+++ 3.Chapter/7.Task/script.js
@@ -1,4 +1,4 @@
-// Temat ćwiczenia: “Wykorzystanie metody abs() do wyznaczenia wartości bezwzględnej”.Napisz skrypt, w którym należy zadeklarować:●zmienną const o nazwie x przechowującą wartość -5,●zmienną const o nazwie y przechowującą wartość 6.Przy pomocy literałów szablonów oraz metody Math.abs() wypisz do konsoli wartości bezwzględne tych dwóch zmiennych tak jak pokazano w oczekiwanym wyniku.Oczekiwany wynik:Wartosc bezwzgledna z - 5 wynosi: 5Wartosc bezwzgledna z 6 wynosi: 6
+/* Temat ćwiczenia: “Wykorzystanie metody abs() do wyznaczenia wartości bezwzględnej”.Napisz skrypt, w którym należy zadeklarować:●zmienną const o nazwie x przechowującą wartość -5,●zmienną const o nazwie y przechowującą wartość 6.Przy pomocy literałów szablonów oraz metody Math.abs() wypisz do konsoli wartości bezwzględne tych dwóch zmiennych tak jak pokazano w oczekiwanym wyniku.Oczekiwany wynik:Wartosc bezwzgledna z - 5 wynosi: 5Wartosc bezwzgledna z 6 wynosi: 6*/
 
 const x = -5;
 const y = 6;
