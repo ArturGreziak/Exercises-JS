a87de353b4c91ac50089be91f3892f90c2c5e076
Artur Gręziak <alex4448@wp.pl>
2022-03-20 13:48:10 +0100

    Nested conditional statement

--- 5.Chapter/7.Task/script.js
+++ 5.Chapter/7.Task/script.js
@@ -1,4 +1,4 @@
-// Temat ćwiczenia: “Zagnieżdżona instrukcja warunkowa”.Napisz skrypt, który sprawdzi czy zadeklarowana zmienna przechowująca wartość - 22 jest dodatnia, ujemna czy też równa się 0.W zależności od wyniku należy wydrukować do konsoli:●Zadeklarowana liczba jest dodatnia●Zadeklarowana liczba jest ujemna●Zadeklarowana liczba to 0Oczekiwany wynik:Zadeklarowana liczba jest ujemna
+// Temat ćwiczenia: “Zagnieżdżona instrukcja warunkowa”.Napisz skrypt, który sprawdzi czy zadeklarowana zmienna przechowująca wartość - 22 jest dodatnia, ujemna czy też równa się 0.W zależności od wyniku należy wydrukować do konsoli:●Zadeklarowana liczba jest dodatnia●Zadeklarowana liczba jest ujemna●Zadeklarowana liczba to 0Oczekiwany wynik:Zadeklarowana liczba jest ujemna.
 
 const liczba = -22;
 
