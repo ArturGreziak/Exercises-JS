2a7a1dfd9273035786b9934a3666ee7a083207f0
Artur Gręziak <alex4448@wp.pl>
2022-03-20 14:03:24 +0100

    Nested conditional statement

--- 5.Chapter/7.Task/script.js
+++ 5.Chapter/7.Task/script.js
@@ -1,11 +1,10 @@
 // Temat ćwiczenia: “Zagnieżdżona instrukcja warunkowa”.Napisz skrypt, który sprawdzi czy zadeklarowana zmienna przechowująca wartość - 22 jest dodatnia, ujemna czy też równa się 0.W zależności od wyniku należy wydrukować do konsoli:●Zadeklarowana liczba jest dodatnia●Zadeklarowana liczba jest ujemna●Zadeklarowana liczba to 0Oczekiwany wynik:Zadeklarowana liczba jest ujemna.
 
-const liczba = -22;
+const liczba = -22; 
 
-if(liczba > 0){
-    console.log(`Zadeklarowana liczba jest dodatnia`);
-} else if (liczba < 0) {
-    console.log(`Zadeklarowana liczba jest ujemna`);
-} else {
-    console.log(`Zadeklarowana liczba to 0`)
-}
+if (liczba > 0) { 
+    console.log(`Zadeklarowana liczba(${liczba}) jest dodatnia`); } 
+    
+    else if(liczba < 0) { console.log(`Zadeklarowana liczba(${liczba}) jest ujemna`); } 
+    
+    else if(liczba === 0) { console.log('Zadeklarowana liczba to 0'); }
