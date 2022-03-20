a4b8763785c86d5c105426d1190451ccb8398908
Artur Gręziak <alex4448@wp.pl>
2022-03-20 11:59:57 +0100

    Conditional statement

---
+++ 5.Chapter/1.Task/script.js
@@ -0,0 +1,9 @@
+// Temat ćwiczenia: “Instrukcja warunkowa”.Napisz skrypt, w którym należy zadeklarować zmienną const przechowującą wartość -17. Zdefiniuj instrukcję warunkową, która będzie wypisywać komunikat ‘Wartosc zmiennej jest ujemna’ w przypadku kiedy wartość zmiennej będzie mniejsza od 0.Oczekiwany wynik:Wartosc zmiennej jest ujemna
+
+const liczba = -17;
+
+
+
+if(liczba < 0){
+    console.log(`Wartosc zmiennej jest ujemna`)
+}
