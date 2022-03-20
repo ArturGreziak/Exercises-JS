1fb2130127b1ce9e0c4f0abc3dbd7fe6132a9be5
Artur Gręziak <alex4448@wp.pl>
2022-03-20 12:16:44 +0100

    Conditional statement

---
+++ 5.Chapter/2.Task/script.js
@@ -0,0 +1,9 @@
+// Temat ćwiczenia: “Instrukcja warunkowa”.Napisz skrypt, w którym należy zadeklarować:●zmienną const o dowolnej nazwie przechowującą wartość 4,●zmienną const o dowolnej nazwie przechowującą wartość 15.Zdefiniuj instrukcję warunkową, która będzie wypisywać komunikat ‘Wartosci zmiennych sa dodatnie’ w momencie kiedy pierwsza oraz druga zmienna będą miały wartości większe od 0.Oczekiwany wynik:Wartosci zmiennych sa dodatnie
+
+const liczba_1 = 4;
+
+const liczba_2 = 15;
+
+if(liczba_1 > 0 && liczba_2 > 0){
+    console.log(`Wartosci zmiennych sa dodatnie`);
+}
